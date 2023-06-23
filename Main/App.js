be582c740f470371822c2550f41b81d7c294b767
Samuel Felipe Verçosa Gonçalves <samuelfelipeif@gmail.com>
2023-06-23 00:03:27 -0300

    feat: rotas

--- Main/App.js
+++ Main/App.js
@@ -1,22 +1,7 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 import Home from "./src/pages/Home/Home";
+import Routes from "./src/Routes";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open App.js to start working on your app!</Text>
-      <Home />
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Routes />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
